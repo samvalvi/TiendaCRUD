357a3fe27f48a9b668412348728419415d5ada10
samvalvi <57542220+samvalvi@users.noreply.github.com>
2021-12-28 13:23:06 -0600

    Add Product function

--- Tienda/Producto.cs
+++ Tienda/Producto.cs
@@ -6,10 +6,29 @@ namespace Tienda
     public partial class Producto
     {
         public int ProductoId { get; set; }
-        public string Nombre { get; set; } = null!;
+        public string Nombre { get; set; }
         public decimal PrecioEspecial { get; set; }
         public decimal PrecioPublico { get; set; }
-        public string Marca { get; set; } = null!;
+        public string Marca { get; set; }
         public int Stock { get; set; }
+
+        public Producto(string nombre, decimal precioEspecial, decimal precioPublico, string marca, int stock)
+        {
+            Nombre = nombre;
+            PrecioEspecial = precioEspecial;
+            PrecioPublico = precioPublico;
+            Marca = marca;
+            Stock = stock;
+        }
+
+        public Producto(int productoId, string nombre, decimal precioEspecial, decimal precioPublico, string marca, int stock)
+        {
+            ProductoId = productoId;
+            Nombre = nombre;
+            PrecioEspecial = precioEspecial;
+            PrecioPublico = precioPublico;
+            Marca = marca;
+            Stock = stock;
+        }
     }
 }
--- TiendaSystem/Program.cs
+++ TiendaSystem/Program.cs
@@ -18,6 +18,7 @@ namespace TiendaSystem
 
             do
             {
+                Console.Clear();
                 Console.WriteLine("Seleccione una opción:");
                 Console.WriteLine("1- Mostrar productos \n2-Insertar producto \n3-Editar producto \n4-Eliminar producto \n5-Salir");
                 op = int.Parse(Console.ReadLine());
@@ -26,6 +27,9 @@ namespace TiendaSystem
                 {
                     case 1:
                         Show(optionsBuilder);
+                        break;
+                    case 2:
+                        Add(optionsBuilder);
                         break;
                     case 5:
                         Environment.Exit(0);
@@ -51,6 +55,31 @@ namespace TiendaSystem
                         $"\nMarca: {producto.Marca} \nPrecio: {producto.PrecioPublico}" + Environment.NewLine);
                 }
             }
+        }
+
+        public static void Add(DbContextOptionsBuilder<TiendaContext>optionsBuilder)
+        {
+            Console.Clear();
+            Console.WriteLine("Agregar producto:");
+            Console.Write("Nombre: ");
+#pragma warning disable CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+            string name = Console.ReadLine();
+#pragma warning restore CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+            Console.Write("Precio especial: ");
+            decimal specialPrice = decimal.Parse(Console.ReadLine());
+            Console.Write("Precio público: ");
+            decimal publicPrice = decimal.Parse(Console.ReadLine());
+            Console.Write("Marca: ");
+#pragma warning disable CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+            string brand = Console.ReadLine();
+#pragma warning restore CS8600 // Se va a convertir un literal nulo o un posible valor nulo en un tipo que no acepta valores NULL
+            Console.Write("Stock: ");
+            int stock = int.Parse(Console.ReadLine());
+
+            using var db = new TiendaContext(optionsBuilder.Options);
+            Producto producto = new Producto(name, specialPrice, publicPrice, brand, stock);
+            db.Add(producto);
+            db.SaveChanges();
         }
     }
 }
